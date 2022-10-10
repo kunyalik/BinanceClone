fc4b67fa4138cadba1906b5943a965fe862b9422
SPatil <sahilvpatil0611@gmail.com>
2022-10-10 21:04:42 +0530

    deleted wrong connection

--- src/db.js
+++
@@ -1,9 +0,0 @@
-var db=require("mysql");
-var connection=db.createConnection({
-	host:"127.0.0.1",
-	user:"kapsas",
-	password:"kapsas",
-	database:"crypbook",
-});
-console.log(connection);
-connection.end();
