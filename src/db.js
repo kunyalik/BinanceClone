06b3a714e234986da809469ef69add1c78c28df6
SPatil <sahilvpatil0611@gmail.com>
2022-10-10 20:53:10 +0530

    mysql connection

---
+++ src/db.js
@@ -0,0 +1,11 @@
+var db = require("mysql");
+
+var connection = db.createConnection({
+    host:"buep3ulcbxagttww4o88-mysql.services.clever-cloud.com",
+    port:"3306",
+    user:"uiwvssjjk04fzpoz",
+    password:"4wEYWvm23zH3SUOqiSxc",
+    database: "buep3ulcbxagttww4o88"
+});
+
+connection.end()
